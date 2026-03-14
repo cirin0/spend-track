9d930687d26b877bc3c57fc27200ea5a3cc3a82d
cirin0 <ivan.gruziv@gmail.com>
2026-03-14 19:41:01 +0200

    update backend services for currency handling

--- app/Http/Requests/Expense/UpdateExpenseRequest.php
+++ app/Http/Requests/Expense/UpdateExpenseRequest.php
@@ -16,6 +16,9 @@ class UpdateExpenseRequest extends FormRequest
         return [
             'category_id' => 'sometimes|required|integer|min:1',
             'amount' => 'sometimes|required|numeric|min:0.01|max:999999.99',
+            'currency' => 'sometimes|required|string|in:UAH,EUR,USD',
+            'converted_amount' => 'sometimes|required|numeric|min:0.01',
+            'exchange_rate' => 'sometimes|required|numeric|min:0.0001',
             'description' => 'nullable|string|max:1000',
             'date' => 'sometimes|required|date|before_or_equal:today',
         ];
@@ -28,6 +31,12 @@ class UpdateExpenseRequest extends FormRequest
             'amount.required' => 'Amount is required',
             'amount.min' => 'Amount must be greater than 0',
             'amount.max' => 'Amount is too large',
+            'currency.required' => 'Currency is required',
+            'currency.in' => 'Currency must be one of: UAH, EUR, USD',
+            'converted_amount.required' => 'Converted amount is required',
+            'converted_amount.min' => 'Converted amount must be greater than 0',
+            'exchange_rate.required' => 'Exchange rate is required',
+            'exchange_rate.min' => 'Exchange rate must be greater than 0',
             'date.required' => 'Date is required',
             'date.before_or_equal' => 'Date cannot be in the future',
         ];
--- app/Http/Requests/Group/StoreGroupExpenseRequest.php
+++ app/Http/Requests/Group/StoreGroupExpenseRequest.php
@@ -16,8 +16,28 @@ class StoreGroupExpenseRequest extends FormRequest
         return [
             'category_id' => 'nullable|integer|exists:group_categories,id',
             'amount' => 'required|numeric|min:0.01|max:9999999.99',
+            'currency' => 'required|string|in:UAH,EUR,USD',
+            'converted_amount' => 'required|numeric|min:0.01',
+            'exchange_rate' => 'required|numeric|min:0.0001',
             'description' => 'nullable|string|max:500',
             'date' => 'required|date|before_or_equal:today'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'amount.required' => 'Amount is required',
+            'amount.min' => 'Amount must be greater than 0',
+            'amount.max' => 'Amount is too large',
+            'currency.required' => 'Currency is required',
+            'currency.in' => 'Currency must be one of: UAH, EUR, USD',
+            'converted_amount.required' => 'Converted amount is required',
+            'converted_amount.min' => 'Converted amount must be greater than 0',
+            'exchange_rate.required' => 'Exchange rate is required',
+            'exchange_rate.min' => 'Exchange rate must be greater than 0',
+            'date.required' => 'Date is required',
+            'date.before_or_equal' => 'Date cannot be in the future',
         ];
     }
 }
